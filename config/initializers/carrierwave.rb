bd8399a260dd88f7aebbcd4bd354d84f88ef522c
Gabriel METZGER <dhc2014.preconsult@gmail.com>
2014-04-07 09:54:23 +0200

    design

---
+++ config/initializers/carrierwave.rb
@@ -0,0 +1,9 @@
+CarrierWave.configure do |config|
+  config.ftp_host = "raspgab.dnsd.me"
+  config.ftp_port = 21
+  config.ftp_user = "pi"
+  config.ftp_passwd = "secret"
+  config.ftp_folder = "/image_bds"
+  config.ftp_url = "http://example.com/uploads"
+  config.passive = false # false by default
+end
